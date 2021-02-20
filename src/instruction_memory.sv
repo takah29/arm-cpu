d7fddc4be4702f9b3e2ae482602b3e1df5b74e3a
takah29 <takah0209@gmail.com>
2021-02-21 00:41:38 +0900

    命令メモリモジュールとテストベンチを追加

---
+++ src/instruction_memory.sv
@@ -0,0 +1,14 @@
+module InstructionMemory(
+    input logic [31:0] address,
+    output logic [31:0] read_data
+    );
+
+    logic [31:0] ram[0:64];
+
+    initial begin
+        $readmemb("program.dat", ram);
+    end
+
+    // 32ビット = 4バイト区切りで指定するので下位2ビットは無視する
+    assign read_data = ram[address[31:2]];
+endmodule
---
+++ test/test_instruction_memory.sv
@@ -0,0 +1,42 @@
+module InstructionMemoryTestbench();
+    logic clk;
+    logic [31:0] address;
+    logic [31:0] read_data, rd_expected;
+    integer errors, vectornum;
+    logic [31:0] testvectors[0:22];
+
+    InstructionMemory dut(.address, .read_data);
+
+    always begin
+        clk = 0;
+        #5;
+        clk = 1;
+        #5;
+    end
+
+    initial begin
+        $readmemb("program.dat", testvectors);
+        vectornum = 0;
+        errors = 0;
+        address = 0;
+    end
+
+    always @(posedge clk) begin
+        #1;
+        rd_expected = testvectors[vectornum];
+    end
+
+    always @(negedge clk) begin
+        if (read_data !== rd_expected) begin
+            $display("Error: input = (%h)", address);
+            $display(" outputs = %h (%h expected)", read_data, rd_expected);
+            errors = errors + 1;
+        end
+        vectornum = vectornum + 1;
+        if (testvectors[vectornum] === 32'bx) begin
+            $display("%d test completed with %d errors", vectornum, errors);
+            $finish;
+        end
+        address = address + 4;
+    end
+endmodule
