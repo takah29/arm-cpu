63f88bdad45158fe8b42c35188747dfbfa47fb58
takah29 <takah0209@gmail.com>
2021-02-20 21:04:45 +0900

    加算器モジュールを追加

---
+++ src/adder.sv
@@ -0,0 +1,11 @@
+module Adder
+    #(parameter N = 8)
+    (
+    input logic [N - 1:0] a, b,
+    input logic cin,
+    output logic [N - 1:0] s,
+    output logic cout
+    );
+
+    assign {cout, s} = a + b + cin;
+endmodule
