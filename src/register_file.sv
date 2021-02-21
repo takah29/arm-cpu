ef320e2596f113fa77c0d0bb8aa9df8fda018457
takah29 <takah0209@gmail.com>
2021-02-21 14:34:07 +0900

    変数名の変更

--- src/register_file.sv
+++ src/register_file.sv
@@ -1,16 +1,16 @@
 module RegisterFile(
     input logic clk, write_enable3,
-    input logic [3:0] read_addr1, read_addr2, write_addr3,
+    input logic [3:0] read_reg_addr1, read_reg_addr2, write_reg_addr3,
     input logic [31:0] write_data3, r15,
     output logic [31:0] read_data1, read_data2
     );
 
     logic [31:0] register_file[0:14];
 
-    assign read_data1 = (read_addr1 == 4'b1111) ? r15 : register_file[read_addr1];
-    assign read_data2 = (read_addr2 == 4'b1111) ? r15 : register_file[read_addr2];
+    assign read_data1 = (read_reg_addr1 == 4'b1111) ? r15 : register_file[read_reg_addr1];
+    assign read_data2 = (read_reg_addr2 == 4'b1111) ? r15 : register_file[read_reg_addr2];
 
     always_ff @(posedge clk) begin
-        if (write_enable3) register_file[write_addr3] <= write_data3;
+        if (write_enable3) register_file[write_reg_addr3] <= write_data3;
     end
 endmodule
--- test/test_register_file.sv
+++ test/test_register_file.sv
@@ -3,12 +3,12 @@ module RegisterFileTestbench();
     parameter STB = 100;
 
     logic clk, write_enable3;
-    logic [3:0] read_addr1, read_addr2, write_addr3;
+    logic [3:0] read_reg_addr1, read_reg_addr2, write_reg_addr3;
     logic [31:0] write_data3, r15;
     logic [31:0] read_data1, read_data2, rd1_expected, rd2_expected;
 
-    RegisterFile dut(.clk, .write_enable3, .read_addr1, .read_addr2,
-    .write_addr3, .write_data3, .r15, .read_data1, .read_data2);
+    RegisterFile dut(.clk, .write_enable3, .read_reg_addr1, .read_reg_addr2,
+    .write_reg_addr3, .write_data3, .r15, .read_data1, .read_data2);
 
     task assert_;
         assert (read_data1 === rd1_expected) else $error("read_data1 = %h, %h expected", read_data1, rd1_expected);
@@ -25,35 +25,35 @@ module RegisterFileTestbench();
     initial begin
         // initialize
         for (int i = 0; i < 15; i++) begin
-            @(posedge clk) write_addr3 = i; write_enable3 = 1; write_data3 = 0; #STB;
+            @(posedge clk) write_reg_addr3 = i; write_enable3 = 1; write_data3 = 0; #STB;
         end
 
         // case1
         @(posedge clk) write_enable3 = 0; write_data3 = 0;
-        read_addr1 = 0; rd1_expected = 0;
-        read_addr2 = 0; rd2_expected = 0;
+        read_reg_addr1 = 0; rd1_expected = 0;
+        read_reg_addr2 = 0; rd2_expected = 0;
         #STB
         assert_;
 
         // case2
-        @(posedge clk) write_enable3 = 1; write_addr3 = 0; write_data3 = 32'hffffffff; #STB;
-        @(posedge clk) write_enable3 = 1; write_addr3 = 14;  write_data3 = 32'hffffffff; #STB;
-        @(posedge clk) write_enable3 = 0; write_addr3 = 0; write_data3 = 32'h0;
-        read_addr1 = 0; rd1_expected = 32'hffffffff;
-        read_addr2 = 14; rd2_expected = 32'hffffffff;
+        @(posedge clk) write_enable3 = 1; write_reg_addr3 = 0; write_data3 = 32'hffffffff; #STB;
+        @(posedge clk) write_enable3 = 1; write_reg_addr3 = 14;  write_data3 = 32'hffffffff; #STB;
+        @(posedge clk) write_enable3 = 0; write_reg_addr3 = 0; write_data3 = 32'h0;
+        read_reg_addr1 = 0; rd1_expected = 32'hffffffff;
+        read_reg_addr2 = 14; rd2_expected = 32'hffffffff;
         #STB;
         assert_;
 
         // case3
-        @(posedge clk) read_addr1 = 15; read_addr2 = 15; r15 = 32'hffffffff;
+        @(posedge clk) read_reg_addr1 = 15; read_reg_addr2 = 15; r15 = 32'hffffffff;
         rd1_expected = 32'hffffffff; rd2_expected = 32'hffffffff;
         #STB;
         assert_;
-        @(posedge clk) read_addr1 = 15; read_addr2 = 15; r15 = 32'h0;
+        @(posedge clk) read_reg_addr1 = 15; read_reg_addr2 = 15; r15 = 32'h0;
         rd1_expected = 32'h0; rd2_expected = 32'h0;
         #STB;
         assert_;
-        @(posedge clk) read_addr2 = 15; read_addr2 = 15; r15 = 32'hffffffff;
+        @(posedge clk) read_reg_addr2 = 15; read_reg_addr2 = 15; r15 = 32'hffffffff;
         rd1_expected = 32'hffffffff; rd2_expected = 32'hffffffff;
         #STB
         assert_;
