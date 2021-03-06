019ae3bf319dc32bf532996e5863b12c344b3160
takah29 <takah0209@gmail.com>
2021-03-07 03:54:21 +0900

    ADD, SUB, AND, ORRの基本テスト追加

--- test/test_arm_cpu.sv
+++ test/test_arm_cpu.sv
@@ -18,11 +18,26 @@ module ArmCpuTestbench;
     .alu_result
     );
 
+    task p(input int x);
+        $display("%1b", x);
+    endtask
+
     task show_regs;
         for (int i = 0; i < 15; i++) begin
             $display("R%1d = %h", i, dut.data_path.register_file.register_file[i]);
         end
         $display("R15 = %h", dut.data_path.register_file.r15);
+    endtask
+
+    task show_flags;
+        $display("pc_src = %b", dut.controller.pc_src);
+        $display("mem_to_reg = %b", dut.controller.mem_to_reg);
+        $display("mem_write = %b", dut.controller.mem_write);
+        $display("alu_ctl = %b", dut.controller.alu_ctl);
+        $display("alu_src = %b", dut.controller.alu_src);
+        $display("imm_src = %b", dut.controller.imm_src);
+        $display("reg_write = %b", dut.controller.reg_write);
+        $display("reg_src = %b", dut.controller.reg_src);
     endtask
 
     task assert_register_value(input logic [3:0] reg_num, input logic [31:0] exp_value);
@@ -66,25 +81,53 @@ module ArmCpuTestbench;
 
         // case: LDR
         // LDR R0, [R14] (データメモリがないのでR14はつかわれない)
-        instr = 32'b11100100000111100000000000000000; read_data = 32'hffffffff;
+        instr = 32'b1110_01_000001_1110_0000_000000000000; read_data = 32'hffffffff;
         @(posedge clk); #DELAY;
         assert_pc(4);
         assert_register_value(0, 32'hffffffff);
 
         // LDR R1, [R14]
-        instr = 32'b11100100000111100001000000000000; read_data = 32'hff;
+        instr = 32'b1110_01_000001_1110_0001_000000000000; read_data = 32'hff;
         @(posedge clk); #DELAY;
         assert_pc(8);
         assert_register_value(1, 32'hff);
 
         // case: STR
         // STR R0, [R1]
-        instr = 32'b11100100000000010000000000000000;
+        instr = 32'b1110_01_000000_0001_0000_000000000000;
         @(posedge clk); #DELAY;
         assert_pc(12);
         assert_alu_result(32'hff);
         assert_write_data(32'hffffffff);
         assert_mem_write(1);
+
+        // ADD R2, R1, R1
+        instr = 32'b1110_00_001000_0001_0010_000000000001;
+        @(posedge clk); #DELAY;
+        assert_pc(16);
+        assert_alu_result(32'h1fe);
+        assert_register_value(2, 32'h1fe);
+
+        // SUB R3, R2, R1
+        instr = 32'b1110_00_000100_0010_0011_000000000001;
+        @(posedge clk); #DELAY;
+        assert_pc(20);
+        assert_alu_result(32'hff);
+        assert_register_value(3, 32'hff);
+
+        // AND R4, R3, R2
+        instr = 32'b1110_00_000000_0011_0100_000000000010;
+        @(posedge clk); #DELAY;
+        assert_pc(24);
+        assert_alu_result(32'hfe);
+        assert_register_value(4, 32'hfe);
+
+        // ORR R5, R3, R2
+        instr = 32'b1110_00_011000_0011_0101_000000000010;
+        @(posedge clk); #DELAY;
+        assert_pc(28);
+        assert_alu_result(32'h1ff);
+        assert_register_value(5, 32'h1ff);
 
         $display("test completed");
         $finish;
