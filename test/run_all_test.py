1ed5f9bf60277539a620100531cf6bf7550ce3b6
takah29 <takah0209@gmail.com>
2021-03-19 22:48:21 +0900

    コンパイル時にすべての警告を表示するようにした

--- test/run_all_test.py
+++ test/run_all_test.py
@@ -11,7 +11,7 @@ def run_test(testbench_filename, dependency_list, root_dir):
         [str(root_dir / "src" / module_filename) for module_filename in dependency_list]
     )
     cmd = (
-        "iverilog -g2012 -o main " f"{str(root_dir / 'test' / testbench_filename)} {dependency_str}"
+        "iverilog -g2012 -Wall -o main " f"{str(root_dir / 'test' / testbench_filename)} {dependency_str}"
     )
     result = subprocess.run(cmd, shell=True)
 
