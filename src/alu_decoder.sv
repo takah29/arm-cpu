c7bf1ecfb91bdcf42bf0b77d55f3b62cbed1776e
takah29 <takah0209@gmail.com>
2021-03-21 02:38:39 +0900

    AluDecoderのalu_ctlフラグを拡張、テスト追加

--- src/alu_decoder.sv
+++ src/alu_decoder.sv
@@ -3,27 +3,29 @@ module AluDecoder
     input logic alu_op, s,
     input logic [3:0] cmd,
     output logic no_write, shift,
-    output logic [1:0] alu_ctl, flag_w
+    output logic [2:0] alu_ctl,
+    output logic [1:0] flag_w
     );
 
     always_comb begin
         if (alu_op) begin
             case (cmd)
-                4'b0100: {alu_ctl, no_write, shift} = 4'b0000; // ADD
-                4'b0010: {alu_ctl, no_write, shift} = 4'b0100; // SUB
-                4'b0000: {alu_ctl, no_write, shift} = 4'b1000; // AND
-                4'b1100: {alu_ctl, no_write, shift} = 4'b1100; // OR
-                4'b1010: {alu_ctl, no_write, shift} = 4'b0110; // CMP
-                4'b1011: {alu_ctl, no_write, shift} = 4'b0010; // CMN
-                4'b1000: {alu_ctl, no_write, shift} = 4'b1010; // TST
-                4'b1101: {alu_ctl, no_write, shift} = 4'bxx01; // LSL
-                default: {alu_ctl, no_write, shift} = 4'bx; // not defined
+                4'b0100: {alu_ctl, no_write, shift} = 5'b000_0_0; // ADD
+                4'b0010: {alu_ctl, no_write, shift} = 5'b001_0_0; // SUB
+                4'b0000: {alu_ctl, no_write, shift} = 5'b010_0_0; // AND
+                4'b1100: {alu_ctl, no_write, shift} = 5'b011_0_0; // OR
+                4'b0101: {alu_ctl, no_write, shift} = 5'b100_0_0; // ADC
+                4'b1010: {alu_ctl, no_write, shift} = 5'b001_1_0; // CMP
+                4'b1011: {alu_ctl, no_write, shift} = 5'b000_1_0; // CMN
+                4'b1000: {alu_ctl, no_write, shift} = 5'b010_1_0; // TST
+                4'b1101: {alu_ctl, no_write, shift} = 5'b0xx_0_1; // LSL
+                default: {alu_ctl, no_write, shift} = 5'bx; // not defined
             endcase
 
             flag_w[1] = s;
-            flag_w[0] = s & (alu_ctl === 2'b00 | alu_ctl === 2'b01);
+            flag_w[0] = s & (alu_ctl === 3'b000 | alu_ctl === 3'b001 | alu_ctl === 3'b100);
         end else begin
-            alu_ctl = 2'b00;
+            alu_ctl = 3'b000;
             flag_w = 2'b00;
             no_write = 1'b0;
             shift = 1'b0;
