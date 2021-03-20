2c03728dc10075933a546973446f1bbce1d9b252
takah29 <takah0209@gmail.com>
2021-03-20 21:54:45 +0900

    出力に色をつけた

--- test/run_all_test.py
+++ test/run_all_test.py
@@ -5,25 +5,32 @@ from pathlib import Path
 import subprocess
 
 
+class Color:
+    RED = "\033[31m"  # (文字)赤
+    GREEN = "\033[32m"  # (文字)緑
+    END = "\033[0m"  # 終了
+
+
 def run_test(testbench_filename, dependency_list, root_dir):
-    print(f"{' Compilation ':=^30}")
+    print(f"{Color.GREEN}{f' Compilation ':=^30}{Color.END}")
     dependency_str = " ".join(
         [str(root_dir / "src" / module_filename) for module_filename in dependency_list]
     )
     cmd = (
-        "iverilog -g2012 -Wall -o main " f"{str(root_dir / 'test' / testbench_filename)} {dependency_str}"
+        "iverilog -g2012 -Wall -o main "
+        f"{str(root_dir / 'test' / testbench_filename)} {dependency_str}"
     )
     result = subprocess.run(cmd, shell=True)
 
     if result.returncode != 0:
-        sys.exit("Compile Error!")
+        sys.exit(f"{Color.RED}Compile Error!{Color.END}")
 
-    print(f"{' Simulation ':=^30}")
+    print(f"{Color.GREEN}{' Simulation ':=^30}{Color.END}")
     cmd = "vvp main"
     result = subprocess.run(cmd, shell=True)
 
     if result.returncode != 0:
-        sys.exit("Simulation Failed!")
+        sys.exit(f"{Color.RED}Simulation Failed!{Color.END}")
 
 
 def main():
@@ -34,7 +41,7 @@ def main():
         dependencies = json.load(f)
 
     for testbench_name, dependency_list in dependencies.items():
-        print(f"Run {testbench_name} ")
+        print(f"{Color.GREEN}Run {testbench_name}{Color.END}")
         run_test(testbench_name + ".sv", dependency_list, root_dir)
         print()
 
