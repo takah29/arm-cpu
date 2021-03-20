ff21b5f8dc8fc49c0ad0db010c294c5903362b3c
takah29 <takah0209@gmail.com>
2021-03-21 03:31:00 +0900

    キャリー付き加算に対応、テスト追加

--- src/alu_with_flag.sv
+++ src/alu_with_flag.sv
@@ -1,20 +1,24 @@
 module AluWithFlag #(parameter N = 32)
     (
     input logic [N -1:0] a, b,
-    input logic [1:0] alu_ctl,
+    input logic [2:0] alu_ctl,
+    input logic carry,
     output logic [N - 1:0] result,
     output logic n, z, c, v
     );
 
     logic [N - 1:0] condinvb;
     logic [N:0] sum;
-    logic cout;
+    logic cout, carry_in;
+    logic [1:0] alu_ctl_low;
 
+    assign carry_in = alu_ctl[2] ? carry : alu_ctl[0];
     assign condinvb = alu_ctl[0] ? ~b : b;
-    assign sum = a + condinvb + alu_ctl[0];
+    assign sum = a + condinvb + carry_in;
+    assign alu_ctl_low = alu_ctl[1:0];
 
     always_comb begin
-        casex (alu_ctl)
+        casex (alu_ctl_low)
             2'b0?: {cout, result} = sum;
             2'b10: result = a & b;
             2'b11: result = a | b;
