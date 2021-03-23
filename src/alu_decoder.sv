c79774b35d8a3d62fd55d66d9c7cc1d9c3eaaaa7
takah29 <takah0209@gmail.com>
2021-03-24 00:04:22 +0900

    RSBに対応、テストを追加

--- src/alu_decoder.sv
+++ src/alu_decoder.sv
@@ -2,7 +2,7 @@ module AluDecoder
     (
     input logic alu_op, s,
     input logic [3:0] cmd,
-    output logic no_write, shift,
+    output logic no_write, shift, swap,
     output logic [2:0] alu_ctl,
     output logic [1:0] flag_w
     );
@@ -10,18 +10,19 @@ module AluDecoder
     always_comb begin
         if (alu_op) begin
             case (cmd)
-                4'b0100: {alu_ctl, no_write, shift} = 5'b000_0_0; // ADD
-                4'b0010: {alu_ctl, no_write, shift} = 5'b001_0_0; // SUB
-                4'b0000: {alu_ctl, no_write, shift} = 5'b010_0_0; // AND
-                4'b1100: {alu_ctl, no_write, shift} = 5'b011_0_0; // OR
-                4'b0001: {alu_ctl, no_write, shift} = 5'b110_0_0; // EOR
-                4'b0101: {alu_ctl, no_write, shift} = 5'b100_0_0; // ADC
-                4'b1010: {alu_ctl, no_write, shift} = 5'b001_1_0; // CMP
-                4'b1011: {alu_ctl, no_write, shift} = 5'b000_1_0; // CMN
-                4'b1000: {alu_ctl, no_write, shift} = 5'b010_1_0; // TST
-                4'b1001: {alu_ctl, no_write, shift} = 5'b110_1_0; // TEQ
-                4'b1101: {alu_ctl, no_write, shift} = 5'b0xx_0_1; // LSL, LSR, ASR, ROR
-                default: {alu_ctl, no_write, shift} = 5'bx; // not defined
+                4'b0100: {alu_ctl, no_write, shift, swap} = 6'b000_0_0_0; // ADD
+                4'b0010: {alu_ctl, no_write, shift, swap} = 6'b001_0_0_0; // SUB
+                4'b0000: {alu_ctl, no_write, shift, swap} = 6'b010_0_0_0; // AND
+                4'b1100: {alu_ctl, no_write, shift, swap} = 6'b011_0_0_0; // OR
+                4'b0001: {alu_ctl, no_write, shift, swap} = 6'b110_0_0_0; // EOR
+                4'b0101: {alu_ctl, no_write, shift, swap} = 6'b100_0_0_0; // ADC
+                4'b0011: {alu_ctl, no_write, shift, swap} = 6'b001_0_0_1; // RSB
+                4'b1010: {alu_ctl, no_write, shift, swap} = 6'b001_1_0_0; // CMP
+                4'b1011: {alu_ctl, no_write, shift, swap} = 6'b000_1_0_0; // CMN
+                4'b1000: {alu_ctl, no_write, shift, swap} = 6'b010_1_0_0; // TST
+                4'b1001: {alu_ctl, no_write, shift, swap} = 6'b110_1_0_0; // TEQ
+                4'b1101: {alu_ctl, no_write, shift, swap} = 6'b0xx_0_1_0; // LSL, LSR, ASR, ROR
+                default: {alu_ctl, no_write, shift, swap} = 6'bx; // not defined
             endcase
 
             flag_w[1] = s;
@@ -31,6 +32,7 @@ module AluDecoder
             flag_w = 2'b00;
             no_write = 1'b0;
             shift = 1'b0;
+            swap = 1'b0;
         end
     end
 endmodule
