eed1affb10d5f389bce2b8cbc88cc77f9d7ba62b
takah29 <takah0209@gmail.com>
2021-03-25 19:40:25 +0900

    AluDecoderにBIC命令に対応、テスト追加

--- src/alu_decoder.sv
+++ src/alu_decoder.sv
@@ -2,7 +2,7 @@ module AluDecoder
     (
     input logic alu_op, s,
     input logic [3:0] cmd,
-    output logic no_write, shift, swap,
+    output logic no_write, shift, swap, inv,
     output logic [2:0] alu_ctl,
     output logic [1:0] flag_w
     );
@@ -10,21 +10,22 @@ module AluDecoder
     always_comb begin
         if (alu_op) begin
             case (cmd)
-                4'b0100: {alu_ctl, no_write, shift, swap} = 6'b000_0_0_0; // ADD
-                4'b0010: {alu_ctl, no_write, shift, swap} = 6'b001_0_0_0; // SUB
-                4'b0000: {alu_ctl, no_write, shift, swap} = 6'b010_0_0_0; // AND
-                4'b1100: {alu_ctl, no_write, shift, swap} = 6'b011_0_0_0; // OR
-                4'b0001: {alu_ctl, no_write, shift, swap} = 6'b110_0_0_0; // EOR
-                4'b0101: {alu_ctl, no_write, shift, swap} = 6'b100_0_0_0; // ADC
-                4'b0110: {alu_ctl, no_write, shift, swap} = 6'b101_0_0_0; // SBC
-                4'b0011: {alu_ctl, no_write, shift, swap} = 6'b001_0_0_1; // RSB
-                4'b0111: {alu_ctl, no_write, shift, swap} = 6'b101_0_0_1; // RSC
-                4'b1010: {alu_ctl, no_write, shift, swap} = 6'b001_1_0_0; // CMP
-                4'b1011: {alu_ctl, no_write, shift, swap} = 6'b000_1_0_0; // CMN
-                4'b1000: {alu_ctl, no_write, shift, swap} = 6'b010_1_0_0; // TST
-                4'b1001: {alu_ctl, no_write, shift, swap} = 6'b110_1_0_0; // TEQ
-                4'b1101: {alu_ctl, no_write, shift, swap} = 6'b0xx_0_1_0; // LSL, LSR, ASR, ROR
-                default: {alu_ctl, no_write, shift, swap} = 6'bx; // not defined
+                4'b0100: {alu_ctl, no_write, shift, swap, inv} = 7'b000_0_0_0_0; // ADD
+                4'b0010: {alu_ctl, no_write, shift, swap, inv} = 7'b001_0_0_0_0; // SUB
+                4'b0000: {alu_ctl, no_write, shift, swap, inv} = 7'b010_0_0_0_0; // AND
+                4'b1100: {alu_ctl, no_write, shift, swap, inv} = 7'b011_0_0_0_0; // OR
+                4'b0001: {alu_ctl, no_write, shift, swap, inv} = 7'b110_0_0_0_0; // EOR
+                4'b0101: {alu_ctl, no_write, shift, swap, inv} = 7'b100_0_0_0_0; // ADC
+                4'b0110: {alu_ctl, no_write, shift, swap, inv} = 7'b101_0_0_0_0; // SBC
+                4'b0011: {alu_ctl, no_write, shift, swap, inv} = 7'b001_0_0_1_0; // RSB
+                4'b0111: {alu_ctl, no_write, shift, swap, inv} = 7'b101_0_0_1_0; // RSC
+                4'b1110: {alu_ctl, no_write, shift, swap, inv} = 7'b010_0_0_0_1; // BIC
+                4'b1010: {alu_ctl, no_write, shift, swap, inv} = 7'b001_1_0_0_0; // CMP
+                4'b1011: {alu_ctl, no_write, shift, swap, inv} = 7'b000_1_0_0_0; // CMN
+                4'b1000: {alu_ctl, no_write, shift, swap, inv} = 7'b010_1_0_0_0; // TST
+                4'b1001: {alu_ctl, no_write, shift, swap, inv} = 7'b110_1_0_0_0; // TEQ
+                4'b1101: {alu_ctl, no_write, shift, swap, inv} = 7'b0xx_0_1_0_0; // LSL, LSR, ASR, ROR
+                default: {alu_ctl, no_write, shift, swap, inv} = 7'bx; // not defined
             endcase
 
             flag_w[1] = s;
@@ -35,6 +36,7 @@ module AluDecoder
             no_write = 1'b0;
             shift = 1'b0;
             swap = 1'b0;
+            inv = 1'b0;
         end
     end
 endmodule
--- test/test_alu_decoder.sv
+++ test/test_alu_decoder.sv
@@ -3,25 +3,26 @@ module AluDecoderTestbench;
 
     logic alu_op, s;
     logic [3:0] cmd;
-    logic no_write, shift, swap;
+    logic no_write, shift, swap, inv;
     logic [1:0] flag_w;
     logic [2:0] alu_ctl;
-    logic no_write_expected, shift_expected, swap_expected;
+    logic no_write_expected, shift_expected, swap_expected, inv_expected;
     logic [1:0] flag_w_expected;
     logic [2:0] alu_ctl_expected;
 
-    AluDecoder dut(.alu_op, .s, .cmd, .no_write, .shift, .swap, .alu_ctl, .flag_w);
+    AluDecoder dut(.alu_op, .s, .cmd, .no_write, .shift, .swap, .inv, .alu_ctl, .flag_w);
 
     task set_exp(
         input logic [2:0] alu_exp_in,
         input logic [1:0] flag_w_exp_in,
-        input logic no_write_exp_in, shift_exp_in, swap_exp_in
+        input logic no_write_exp_in, shift_exp_in, swap_exp_in, inv_exp_in
         );
         alu_ctl_expected <= alu_exp_in;
         flag_w_expected <= flag_w_exp_in;
         no_write_expected <= no_write_exp_in;
         shift_expected <= shift_exp_in;
         swap_expected <= swap_exp_in;
+        inv_expected <= inv_exp_in;
     endtask
 
     task assert_;
@@ -30,12 +31,13 @@ module AluDecoderTestbench;
         assert (no_write === no_write_expected) else $error("no_write = %b, %b expected", no_write, no_write_expected);
         assert (shift === shift_expected) else $error("shift = %b, %b expected", shift, shift_expected);
         assert (swap === swap_expected) else $error("swap = %b, %b expected", swap, swap_expected);
+        assert (inv === inv_expected) else $error("inv = %b, %b expected", inv, inv_expected);
     endtask
 
     initial begin
         // case1: not defined
         alu_op = '0; s = '1; cmd = 4'b0100;
-        set_exp(3'b000, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b000, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         //alu_op = 1
@@ -43,102 +45,110 @@ module AluDecoderTestbench;
 
         // case ADD
         s = '0; cmd = 4'b0100;
-        set_exp(3'b000, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b000, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b000, 2'b11, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b000, 2'b11, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case SUB
         s = '0; cmd = 4'b0010;
-        set_exp(3'b001, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b001, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b001, 2'b11, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b001, 2'b11, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case AND
         s = '0; cmd = 4'b0000;
-        set_exp(3'b010, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b010, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b010, 2'b10, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b010, 2'b10, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case OR
         s = '0; cmd = 4'b1100;
-        set_exp(3'b011, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b011, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b011, 2'b10, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b011, 2'b10, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case EOR
         s = '0; cmd = 4'b0001;
-        set_exp(3'b110, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b110, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b110, 2'b10, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b110, 2'b10, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case ADC
         s = '0; cmd = 4'b0101;
-        set_exp(3'b100, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b100, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b100, 2'b11, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b100, 2'b11, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case SBC
         s = '0; cmd = 4'b0110;
-        set_exp(3'b101, 2'b00, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b101, 2'b00, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b101, 2'b11, 1'b0, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b101, 2'b11, 1'b0, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case RSB
         s = '0; cmd = 4'b0011;
-        set_exp(3'b001, 2'b00, 1'b0, 1'b0, 1'b1); #DELAY;
+        set_exp(3'b001, 2'b00, 1'b0, 1'b0, 1'b1, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b001, 2'b11, 1'b0, 1'b0, 1'b1); #DELAY;
+        set_exp(3'b001, 2'b11, 1'b0, 1'b0, 1'b1, 1'b0); #DELAY;
         assert_;
 
         // case RSC
         s = '0; cmd = 4'b0111;
-        set_exp(3'b101, 2'b00, 1'b0, 1'b0, 1'b1); #DELAY;
+        set_exp(3'b101, 2'b00, 1'b0, 1'b0, 1'b1, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b101, 2'b11, 1'b0, 1'b0, 1'b1); #DELAY;
+        set_exp(3'b101, 2'b11, 1'b0, 1'b0, 1'b1, 1'b0); #DELAY;
+        assert_;
+
+        // case BIC
+        s = '0; cmd = 4'b1110;
+        set_exp(3'b010, 2'b00, 1'b0, 1'b0, 1'b0, 1'b1); #DELAY;
+        assert_;
+        s = '1;
+        set_exp(3'b010, 2'b10, 1'b0, 1'b0, 1'b0, 1'b1); #DELAY;
         assert_;
 
         // case CMP
         s = '1; cmd = 4'b1010;
-        set_exp(3'b001, 2'b11, 1'b1, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b001, 2'b11, 1'b1, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case CMN
         s = '1; cmd = 4'b1011;
-        set_exp(3'b000, 2'b11, 1'b1, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b000, 2'b11, 1'b1, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case TST
         s = '1; cmd = 4'b1000;
-        set_exp(3'b010, 2'b10, 1'b1, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b010, 2'b10, 1'b1, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case TEQ
         s = '1; cmd = 4'b1001;
-        set_exp(3'b110, 2'b10, 1'b1, 1'b0, 1'b0); #DELAY;
+        set_exp(3'b110, 2'b10, 1'b1, 1'b0, 1'b0, 1'b0); #DELAY;
         assert_;
 
         // case LSL, LSR, ASR, ROR
         s = '0; cmd = 4'b1101;
-        set_exp(3'b0xx, 2'b00, 1'b0, 1'b1, 1'b0); #DELAY;
+        set_exp(3'b0xx, 2'b00, 1'b0, 1'b1, 1'b0, 1'b0); #DELAY;
         assert_;
         s = '1;
-        set_exp(3'b0xx, 2'b10, 1'b0, 1'b1, 1'b0); #DELAY;
+        set_exp(3'b0xx, 2'b10, 1'b0, 1'b1, 1'b0, 1'b0); #DELAY;
         assert_;
 
         $display("test completed");
