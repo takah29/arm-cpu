5790bb3a8a655ed7cd9890dbc4fb42243ff3dd5c
takah29 <takah0209@gmail.com>
2021-03-27 01:37:08 +0900

    RegisterFileにRD3を追加したのでそれの伴いテストを修正

--- src/register_file.sv
+++ src/register_file.sv
@@ -3,13 +3,14 @@ module RegisterFile(
     input logic write_enable3,
     input logic [3:0] read_reg_addr1, read_reg_addr2, write_reg_addr3, read_reg_addrs,
     input logic [31:0] write_data3, r15,
-    output logic [31:0] read_data1, read_data2, read_datas
+    output logic [31:0] read_data1, read_data2, read_data3, read_datas
     );
 
     logic [31:0] reg_file[0:14];
 
     assign read_data1 = (read_reg_addr1 == 4'b1111) ? r15 : reg_file[read_reg_addr1];
     assign read_data2 = (read_reg_addr2 == 4'b1111) ? r15 : reg_file[read_reg_addr2];
+    assign read_data3 = (write_reg_addr3 == 4'b1111) ? r15 : reg_file[write_reg_addr3];
     assign read_datas = (read_reg_addrs == 4'b1111) ? r15 : reg_file[read_reg_addrs];
 
     always_ff @(posedge clk, posedge reset) begin
