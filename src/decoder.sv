1a0d0d5df9d3e67d9734da420f84c3ece4733c3a
takah29 <takah0209@gmail.com>
2021-03-28 01:08:33 +0900

    post_idxフラグをshiftとまとめてresult_srcにまとめた、テスト修正

--- src/decoder.sv
+++ src/decoder.sv
@@ -3,23 +3,23 @@ module Decoder
     input logic [1:0] op,
     input logic [5:0] funct,
     input logic [3:0] rd,
-    output logic pcs, reg_w, mem_w, mem_to_reg, alu_src, reg_src, no_write, shift, swap, inv,
-    output logic [1:0] flag_w, imm_src,
+    output logic pcs, reg_w, mem_w, mem_to_reg, alu_src, reg_src, no_write, swap, inv,
+    output logic [1:0] flag_w, imm_src, result_src,
     output logic [2:0] alu_ctl
     );
 
-    logic branch, alu_op;
+    logic branch, alu_op, shift, post_idx;
 
     MainDecoder main_decoder(
     .op,
-    .funct_5(funct[5]),
-    .funct_0(funct[0]),
+    .funct,
     .branch,
     .mem_to_reg,
     .mem_w,
     .alu_src,
     .reg_w,
     .alu_op,
+    .post_idx,
     .imm_src,
     .reg_src
     );
@@ -37,4 +37,5 @@ module Decoder
     );
 
     assign pcs = ((rd == 15) & reg_w) | branch;
+    assign result_src = {post_idx, shift};
 endmodule
