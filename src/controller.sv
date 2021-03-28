de70a796265b97dc8879208ad9545611c5fa518f
takah29 <takah0209@gmail.com>
2021-03-29 01:08:44 +0900

    Controllerにbase_reg_writeフラグを追加、テスト追加

--- src/controller.sv
+++ src/controller.sv
@@ -4,7 +4,7 @@ module Controller
     input logic [1:0] op,
     input logic [3:0] cond, alu_flags, rd,
     input logic [5:0] funct,
-    output logic pc_src, reg_write, mem_write, mem_to_reg, alu_src, reg_src, carry, swap, inv,
+    output logic pc_src, reg_write, mem_write, mem_to_reg, alu_src, reg_src, base_reg_write, carry, swap, inv,
     output logic [1:0] imm_src, result_src,
     output logic [2:0] alu_ctl
     );
@@ -28,6 +28,7 @@ module Controller
     .imm_src,
     .result_src,
     .reg_src,
+    .base_reg_write,
     .alu_ctl
     );
 
