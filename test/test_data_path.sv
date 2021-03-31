1909a7b91e7c923b8fcb2774e4d7b768055e96c9
takah29 <takah0209@gmail.com>
2021-04-01 00:19:53 +0900

    乗算命令用に回路を追加、テストも追加した

--- test/test_data_path.sv
+++ test/test_data_path.sv
@@ -3,10 +3,11 @@ module DataPathTestbench();
     parameter STB = 100;
 
     logic clk, reset;
-    logic pc_src, reg_write, base_reg_write, mem_to_reg, alu_src, reg_src, carry, swap, inv;
+    logic pc_src, reg_write3, reg_write1, mem_to_reg, alu_src, reg_src, carry, swap, inv;
     logic [31:0] instr, read_data;
     logic [1:0] imm_src, result_src;
     logic [2:0] alu_ctl;
+    logic [3:0] mul_ctl;
     logic [31:0] pc, write_data, data_memory_addr;
     logic [31:0] pc_exp, write_data_exp, data_memory_addr_exp;
 
@@ -16,8 +17,8 @@ module DataPathTestbench();
     .pc_src,
     .imm_src,
     .result_src,
-    .reg_write,
-    .base_reg_write,
+    .reg_write3,
+    .reg_write1,
     .mem_to_reg,
     .alu_src,
     .carry,
@@ -26,6 +27,7 @@ module DataPathTestbench();
     .instr,
     .read_data,
     .alu_ctl,
+    .mul_ctl,
     .reg_src,
     .pc,
     .write_data,
@@ -57,30 +59,31 @@ module DataPathTestbench();
         carry = 1'b0;
         swap = 1'b0;
         inv = 1'b0;
-        base_reg_write = 1'b0;
+        reg_write1 = 1'b0;
+        mul_ctl = '0;
 
         // case1: r1に15を設定、r11にアドレス32を設定、r1の値をアドレスr11に書き込む
         // ldr r1, [r0] （read_dataで設定するのでinstrのr0のところは何でもいい）
-        instr = 32'b11100101100100000001000000000000; read_data = 15; reg_write = 1;
+        instr = 32'b11100101100100000001000000000000; read_data = 15; reg_write3 = 1;
         @(posedge clk);
         #STB;
         assert (dut.register_file.reg_file[1] === read_data);
 
         // ldr r11, [r0] （read_dataで設定するのでinstrのr0のところは何でもいい）
-        instr = 32'b11100101100100001011000000000000; read_data = 32; reg_write = 1;
+        instr = 32'b11100101100100001011000000000000; read_data = 32; reg_write3 = 1;
         @(posedge clk);
         #STB;
         assert (dut.register_file.reg_file[11] === read_data);
 
         // str r1. [r11]
-        instr = 32'b11100101100010110001000000000000; read_data = 0; reg_write = 0;
+        instr = 32'b11100101100010110001000000000000; read_data = 0; reg_write3 = 0;
         @(posedge clk);
         #STB;
         write_data_exp = 15; data_memory_addr_exp = 32;
         assert_out;
 
         // case2: プログラムカウンタに128を設定して、r15に 128 + 8 = 136 が設定されることを確認
-        instr = 32'b11100101100011110000000000000000; read_data = 128; reg_write = 0; pc_src = 1;
+        instr = 32'b11100101100011110000000000000000; read_data = 128; reg_write3 = 0; pc_src = 1;
         @(posedge clk);
         #STB;
         assert (dut.register_file.read_data1 === 136);
