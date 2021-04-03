8e6048b2580d5b37ce51c03307dc939a6872eb09
takah29 <takah0209@gmail.com>
2021-04-03 23:21:59 +0900

    出力値をPC + 4に変更、テスト修正

--- test/test_pc_module.sv
+++ test/test_pc_module.sv
@@ -3,14 +3,14 @@ module PcModuleTestbench();
     parameter STB = 100;
 
     logic clk, reset, pc_src;
-    logic [31:0] jump, pc, pc_plus8;
-    logic [31:0] pc_expected, pc_plus8_expected;
+    logic [31:0] jump, pc, pc_plus4;
+    logic [31:0] pc_expected, pc_plus4_expected;
 
-    PcModule dut(.clk, .reset, .pc_src, .jump, .pc, .pc_plus8);
+    PcModule dut(.clk, .reset, .pc_src, .jump, .pc, .pc_plus4);
 
     task assert_;
         assert (pc === pc_expected) else $error("pc = %h, %h expected", pc, pc_expected);
-        assert (pc_plus8 === pc_plus8_expected) else $error("pc_plus8 = %h, %h expected", pc_plus8, pc_plus8_expected);
+        assert (pc_plus4 === pc_plus4_expected) else $error("pc_plus4 = %h, %h expected", pc_plus4, pc_plus4_expected);
     endtask
 
     always begin
@@ -22,19 +22,19 @@ module PcModuleTestbench();
 
     initial begin
         // PC初期化
-        pc_src = 1; jump = 0; pc_expected = 0; pc_plus8_expected = 8;
+        pc_src = 1; jump = 0; pc_expected = 0; pc_plus4_expected = 4;
         @(posedge clk)
         #STB;
         assert_;
 
         // カウントアップ
-        pc_src = 0; jump = 0; pc_expected = 4; pc_plus8_expected = 12;
+        pc_src = 0; jump = 0; pc_expected = 4; pc_plus4_expected = 8;
         @(posedge clk)
         #STB;
         assert_;
 
         // ジャンプ
-        pc_src = 1; jump = 1024; pc_expected = 1024; pc_plus8_expected = 1032;
+        pc_src = 1; jump = 1024; pc_expected = 1024; pc_plus4_expected = 1028;
         @(posedge clk)
         #STB;
         assert_;
