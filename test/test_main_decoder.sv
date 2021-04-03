5e6bd4607a376ac1bf066a2172ccb490887ee039
takah29 <takah0209@gmail.com>
2021-04-03 23:01:45 +0900

    MainDecoderをBL命令に対応、テスト追加

--- test/test_main_decoder.sv
+++ test/test_main_decoder.sv
@@ -4,10 +4,10 @@ module MainDecoderTestbench;
     logic [1:0] op;
     logic [5:0] funct;
     logic [3:0] instr74;
-    logic branch, mem_to_reg, mem_w, alu_src, reg_src, reg_w3, reg_w1, alu_op, post_idx, mult;
-    logic [1:0] imm_src;
-    logic branch_exp, mem_to_reg_exp, mem_w_exp, alu_src_exp, reg_src_exp, reg_w3_exp, reg_w1_exp, alu_op_exp, post_idx_exp, mult_exp;
-    logic [1:0] imm_src_exp;
+    logic branch, mem_to_reg, mem_w, alu_src, reg_w3, reg_w1, alu_op, post_idx, mult;
+    logic [1:0] imm_src, reg_src;
+    logic branch_exp, mem_to_reg_exp, mem_w_exp, alu_src_exp, reg_w3_exp, reg_w1_exp, alu_op_exp, post_idx_exp, mult_exp;
+    logic [1:0] imm_src_exp, reg_src_exp;
 
     MainDecoder dut(
     .op,
@@ -40,7 +40,7 @@ module MainDecoderTestbench;
         assert (mult === mult_exp) else $error("mult = %b, %b expected", mult, mult_exp);
     endtask
 
-    task set_exp(input [11:0] controls);
+    task set_exp(input [12:0] controls);
         begin
             {
                 branch_exp,
@@ -60,75 +60,79 @@ module MainDecoderTestbench;
 
     initial begin
         // case: type DP Reg
-        op = 2'b00; funct = 6'b000000; instr74 = 4'b0001; set_exp(12'b0_0_0_0_00_1_0_0_1_0_0); #DELAY;
+        op = 2'b00; funct = 6'b000000; instr74 = 4'b0001; set_exp(13'b0_0_0_0_00_1_0_00_1_0_0); #DELAY;
         assert_;
 
         // case: type DP Imm
-        op = 2'b00; funct = 6'b100000; instr74 = 4'b0000; set_exp(12'b0_0_0_1_00_1_0_0_1_0_0); #DELAY;
+        op = 2'b00; funct = 6'b100000; instr74 = 4'b0000; set_exp(13'b0_0_0_1_00_1_0_00_1_0_0); #DELAY;
         assert_;
 
         // case: DP Multiply 32-bit
-        op = 2'b00; funct = 6'b000000; instr74 = 4'b1001; set_exp(12'b0_0_0_0_00_0_1_0_0_0_1); #DELAY;
+        op = 2'b00; funct = 6'b000000; instr74 = 4'b1001; set_exp(13'b0_0_0_0_00_0_1_00_0_0_1); #DELAY;
         assert_;
 
         // case: DP Multiply 64-bit
-        op = 2'b00; funct = 6'b001000; instr74 = 4'b1001; set_exp(12'b0_0_0_0_00_1_1_0_0_0_1); #DELAY;
+        op = 2'b00; funct = 6'b001000; instr74 = 4'b1001; set_exp(13'b0_0_0_0_00_1_1_00_0_0_1); #DELAY;
         assert_;
 
         // case: type STR (Imm)
-        op = 2'b01; funct = 6'b01XX00; instr74 = 4'b1001; set_exp(12'b0_0_1_1_01_0_0_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b01XX00; instr74 = 4'b1001; set_exp(13'b0_0_1_1_01_0_0_00_0_0_0); #DELAY;
         assert_;
 
         // case: type STR (Reg)
-        op = 2'b01; funct = 6'b11XX00; instr74 = 4'b0000; set_exp(12'b0_0_1_0_01_0_0_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b11XX00; instr74 = 4'b0000; set_exp(13'b0_0_1_0_01_0_0_00_0_0_0); #DELAY;
         assert_;
 
         // case: type STR (Imm, PostIdx)
-        op = 2'b01; funct = 6'b00XX00; instr74 = 4'b0000; set_exp(12'b0_0_1_1_01_0_1_0_0_1_0); #DELAY;
+        op = 2'b01; funct = 6'b00XX00; instr74 = 4'b0000; set_exp(13'b0_0_1_1_01_0_1_00_0_1_0); #DELAY;
         assert_;
 
         // case: type STR (Reg, PostIdx)
-        op = 2'b01; funct = 6'b10XX00; instr74 = 4'b0000; set_exp(12'b0_0_1_0_01_0_1_0_0_1_0); #DELAY;
+        op = 2'b01; funct = 6'b10XX00; instr74 = 4'b0000; set_exp(13'b0_0_1_0_01_0_1_00_0_1_0); #DELAY;
         assert_;
 
         // case: type STR (Imm, PreIdx)
-        op = 2'b01; funct = 6'b01XX10; instr74 = 4'b0000; set_exp(12'b0_0_1_1_01_0_1_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b01XX10; instr74 = 4'b0000; set_exp(13'b0_0_1_1_01_0_1_00_0_0_0); #DELAY;
         assert_;
 
         // case: type STR (Reg, PreIdx)
-        op = 2'b01; funct = 6'b11XX10; instr74 = 4'b0000; set_exp(12'b0_0_1_0_01_0_1_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b11XX10; instr74 = 4'b0000; set_exp(13'b0_0_1_0_01_0_1_00_0_0_0); #DELAY;
         assert_;
 
         // case: type LDR (Imm)
-        op = 2'b01; funct = 6'b01XX01; instr74 = 4'b0000; set_exp(12'b0_1_0_1_01_1_0_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b01XX01; instr74 = 4'b0000; set_exp(13'b0_1_0_1_01_1_0_00_0_0_0); #DELAY;
         assert_;
 
         // case: type LDR (Reg)
-        op = 2'b01; funct = 6'b11XX01; instr74 = 4'b0000; set_exp(12'b0_1_0_0_01_1_0_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b11XX01; instr74 = 4'b0000; set_exp(13'b0_1_0_0_01_1_0_00_0_0_0); #DELAY;
         assert_;
 
         // case: type LDR (Imm, post_idx)
-        op = 2'b01; funct = 6'b00XX01; instr74 = 4'b0000; set_exp(12'b0_1_0_1_01_1_1_0_0_1_0); #DELAY;
+        op = 2'b01; funct = 6'b00XX01; instr74 = 4'b0000; set_exp(13'b0_1_0_1_01_1_1_00_0_1_0); #DELAY;
         assert_;
 
         // case: type LDR (Reg, post_idx)
-        op = 2'b01; funct = 6'b10XX01; instr74 = 4'b0000; set_exp(12'b0_1_0_0_01_1_1_0_0_1_0); #DELAY;
+        op = 2'b01; funct = 6'b10XX01; instr74 = 4'b0000; set_exp(13'b0_1_0_0_01_1_1_00_0_1_0); #DELAY;
         assert_;
 
         // case: type LDR (Imm, PreIdx)
-        op = 2'b01; funct = 6'b01XX11; instr74 = 4'b0000; set_exp(12'b0_1_0_1_01_1_1_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b01XX11; instr74 = 4'b0000; set_exp(13'b0_1_0_1_01_1_1_00_0_0_0); #DELAY;
         assert_;
 
         // case: type LDR (Reg, PreIdx)
-        op = 2'b01; funct = 6'b11XX11; instr74 = 4'b0000; set_exp(12'b0_1_0_0_01_1_1_0_0_0_0); #DELAY;
+        op = 2'b01; funct = 6'b11XX11; instr74 = 4'b0000; set_exp(13'b0_1_0_0_01_1_1_00_0_0_0); #DELAY;
         assert_;
 
         // case: type B
-        op = 2'b10; funct = 6'b100000; instr74 = 4'b0000; set_exp(12'b1_0_0_1_10_0_0_1_0_0_0); #DELAY;
+        op = 2'b10; funct = 6'b100000; instr74 = 4'b0000; set_exp(13'b1_0_0_1_10_0_0_01_0_0_0); #DELAY;
+        assert_;
+
+        // case: type BL
+        op = 2'b10; funct = 6'b110000; instr74 = 4'b0000; set_exp(13'b1_0_0_1_10_1_0_11_0_0_0); #DELAY;
         assert_;
 
         // case: type BX
-        op = 2'b00; funct = 6'b010010; instr74 = 4'b0001; set_exp(12'b1_0_0_0_00_0_0_0_0_0_0); #DELAY;
+        op = 2'b00; funct = 6'b010010; instr74 = 4'b0001; set_exp(13'b1_0_0_0_00_0_0_00_0_0_0); #DELAY;
         assert_;
 
         $display("test completed");
