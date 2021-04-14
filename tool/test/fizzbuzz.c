143c31855b7f6f4f324a5a7fe35f9664aacc8bb1
takah29 <takah0209@gmail.com>
2021-04-14 20:28:12 +0900

    バグ修正

--- tool/test/fizzbuzz.c
+++ tool/test/fizzbuzz.c
@@ -2,12 +2,12 @@ int main() {
     int i;
     int ret[50];
     for (i = 1; i < 50; i++) {
-        if (i % 3 == 0) {
-            ret[i] = 1;
+        if (i % 15 == 0) {
+            ret[i] = 3;
         } else if (i % 5 == 0) {
             ret[i] = 2;
-        } else if (i % 15 == 0) {
-            ret[i] = 3;
+        } else if (i % 3 == 0) {
+            ret[i] = 1;
         } else {
             ret[i] = 0;
         }
