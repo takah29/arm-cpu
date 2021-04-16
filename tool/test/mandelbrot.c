6038e9d974d6f62e2e0d12a162db1ec6f59114c1
takah29 <takah0209@gmail.com>
2021-04-17 02:46:28 +0900

    マンデルブロ集合を計算するテストプログラムを追加

---
+++ tool/test/mandelbrot.c
@@ -0,0 +1,31 @@
+float CENTER_X = -0.5;
+float CENTER_Y = 0.0;
+float SCALE_X = 2.0;
+float SCALE_Y = 2.0;
+int X_SIZE = 100;
+int Y_SIZE = 100;
+
+int main() {
+    int image[Y_SIZE][X_SIZE];
+    float y = 0, x = 0;
+
+    y = CENTER_Y + SCALE_Y / 2.0;
+    for (int i = 0; i < Y_SIZE; i++) {
+        y -= SCALE_Y / Y_SIZE;
+        x = CENTER_X + SCALE_X / 2.0;
+        for (int j = 0; j < X_SIZE; j++) {
+            x += SCALE_X / X_SIZE;
+            float z1 = 0.0, z2 = 0.0;
+            for (int n = 1; n < 1000; n++) {
+                z1 = z1 * z1 - z2 * z2 + x;
+                z2 = 2 * z1 * z2 + y;
+                if (z1 * z1 + z2 * z2 > 2.0) {
+                    image[i][j] = 255;
+                }
+            }
+            image[i][j] = 0;
+        }
+    }
+
+    return 0;
+}
