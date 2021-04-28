81039a7de1165e623f8fc7dffd7402820378d94c
takah29 <takah0209@gmail.com>
2021-04-29 02:41:20 +0900

    バグ修正と仕様変更

--- tool/get_instruction_data.py
+++ tool/get_instruction_data.py
@@ -32,7 +32,7 @@ def push_to_strs(instr):
     # use only push(stm) instructions.
     result = []
     reg_list = get_register_list(instr)
-    n_regs = sum(reg_list)
+    n_regs = len(reg_list)
 
     # sub instruction
     bin_rep = f"1110_00_100100_1101_1101_0000_{bin(4 * n_regs)[2:].zfill(8)}"
@@ -42,7 +42,7 @@ def push_to_strs(instr):
     # str instructions
     for i, rd in enumerate(reg_list[::-1], start=1):
         bin_rep = (
-            f"1110_10_011000_1101_{bin(rd)[2:].zfill(4)}_{bin(4 * (n_regs - i))[2:].zfill(12)}"
+            f"1110_01_011000_1101_{bin(rd)[2:].zfill(4)}_{bin(4 * (n_regs - i))[2:].zfill(12)}"
         )
         instr = int(bin_rep.replace("_", ""), 2)
         result.append(instr)
@@ -54,11 +54,11 @@ def pop_to_ldrs(instr):
     # use only pop(ldm) instructions.
     result = []
     reg_list = get_register_list(instr)
-    n_regs = sum(reg_list)
+    n_regs = len(reg_list)
 
-    # str instructions
+    # ldr instructions
     for i, rd in enumerate(reg_list, start=0):
-        bin_rep = f"1110_10_011001_1101_{bin(rd)[2:].zfill(4)}_{bin(4 * i)[2:].zfill(12)}"
+        bin_rep = f"1110_01_011001_1101_{bin(rd)[2:].zfill(4)}_{bin(4 * i)[2:].zfill(12)}"
         instr = int(bin_rep.replace("_", ""), 2)
         result.append(instr)
 
@@ -91,7 +91,6 @@ def binary_to_text(from_file, to_file):
         tmp = f.read()
 
     data = unpack("<" + "I" * (len(tmp) // 4), tmp)
-    addr_offset = 0
     with open(to_file, "w") as f:
         for instr in data:
             # push, pop命令は非対応なので、それぞれsub, addでspだけ移動する命令に置き換える
@@ -103,38 +102,55 @@ def binary_to_text(from_file, to_file):
 
                 for instr in instr_list:
                     f.write(f"{to_hex_string(instr)}\n")
-                addr_offset += sum(instr_list) - 1
             elif is_ldm(instr):  # pop命令を複数のldrに置き換えてアドレスのオフセット値を追加する
                 print("Replaced pop(ldm) with add.")
                 instr_list = pop_to_ldrs(instr)
 
                 for instr in instr_list:
                     f.write(f"{to_hex_string(instr)}\n")
-                addr_offset += sum(instr_list) - 1
-            elif is_branch(instr):  # branch命令にオフセットを追加する
-                print("Added an offset to the branch.")
-                instr = offset_branch_label(instr, addr_offset)
-                f.write(f"{to_hex_string(instr)}\n")
             else:
                 f.write(f"{to_hex_string(instr)}\n")
+
+
+def compile_source(filepath):
+    # clangでデバッグ情報を埋め込んでオブジェクトファイルを作成する
+    # cmd_clang = f"clang -O0 -target armv5 -mfloat-abi=soft -g -c {filepath}"
+    cmd_gcc = [
+        "arm-none-eabi-gcc",
+        "-O0",
+        "--specs=nosys.specs",
+        "-march=armv5t",
+        "-mfloat-abi=soft",
+        "-g",
+        "-c",
+        filepath,
+    ]
+    subprocess.run(cmd_gcc)
 
 
 def main(args):
     filepath = Path(args.filepath)
 
-    # clangでデバッグ情報を埋め込んでオブジェクトファイルを作成する
-    cmd_clang = f"clang -O0 -target armv5 -mfloat-abi=soft -g -c {filepath}"
-    # cmd_gcc = f"arm-none-eabi-gcc -O0 --specs=nosys.specs -march=armv5t -mfloat-abi=soft -g -c {filepath}"
-    subprocess.run(cmd_clang.split(" "))
+    # ファイル形式チェック
+    if args.exe_file:
+        if filepath.suffix != ".out":  # 実行ファイル
+            raise NotImplementedError("The extension must be .out.")
+    else:
+        if filepath.suffix != ".c":  # ソースファイル
+            raise NotImplementedError("The extension must be .c.")
+
+    if not args.exe_file:
+        compile_source(str(filepath))
+        filepath = Path(f"{filepath.stem}.o")
 
     try:
         if args.debug:
             # オブジェクトファイルを逆アセンブルする
-            cmd = f"llvm-objdump -S {filepath.stem}.o"
+            cmd = f"llvm-objdump -S {str(filepath)}"
             subprocess.run(cmd.split(" "))
         else:
             # オブジェクトファイルから命令部だけをテキストとして取り出す
-            cmd = f"llvm-objcopy {filepath.stem}.o -O binary program.bin"
+            cmd = f"llvm-objcopy -O binary --only-section=.text {str(filepath)} program.bin"
             subprocess.run(cmd.split(" "))
 
             binary_to_text("program.bin", "program.dat")
@@ -153,6 +169,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compile C-source with clang")
     parser.add_argument("filepath", help="C-source filepath")
     parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("-e", "--exe_file", action="store_true")
 
     args = parser.parse_args()
 
