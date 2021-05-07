ef1c9d3db7c5f5600999a52f91750737755792a0
takah29 <takah0209@gmail.com>
2021-05-07 18:58:35 +0900

    微修正

--- src/data_memory.sv
+++ src/data_memory.sv
@@ -6,12 +6,11 @@ module DataMemory(
 
     logic [31:0] ram[0:1048575]; // 4MB
 
+    // 32ビット = 4バイト区切りで指定するので下位2ビットは無視する
     assign read_data = ram[address[31:2]];
 
     always_ff @(posedge clk) begin
         if (write_enable) ram[address[31:2]] <= write_data;
     end
-
-    // 32ビット = 4バイト区切りで指定するので下位2ビットは無視する
 
 endmodule
