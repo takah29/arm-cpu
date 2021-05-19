c5fbbf61a7d7f80350103d07de70d42f9b320822
takah29 <takah0209@gmail.com>
2021-05-19 23:26:00 +0900

    計算時間短縮のため出力画像サイズを小さくした

--- tool/test/mandelbrot.c
+++ tool/test/mandelbrot.c
@@ -1,6 +1,6 @@
 int main() {
-    int X_SIZE = 800;
-    int Y_SIZE = 800;
+    int X_SIZE = 10;
+    int Y_SIZE = 10;
     float image[Y_SIZE][X_SIZE];
 
     float CENTER_X = -0.5;
@@ -20,7 +20,7 @@ int main() {
             x += dx;
             image[i][j] = 0;
             float z1 = 0, z2 = 0, z1_ = 0, z2_ = 0;
-            for (int n = 0; n < 100; n++) {
+            for (int n = 0; n < 20; n++) {
                 z1_ = z1 * z1 - z2 * z2 + x;
                 z2_ = 2 * z1 * z2 + y;
                 if (z1_ * z1_ + z2_ * z2_ > 4.0) {
